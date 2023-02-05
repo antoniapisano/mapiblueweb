6ad2c76fe2b195c3ca09c393df4aaf5ceb896e32
antoniapisano <antoniapisano@campus.masterschool.com>
2023-02-05 21:24:17 +0000

    almost completed home layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import Layout from './components/layout'
 import { Six_Caps } from '@next/font/google'
 
+
 const sixcaps = Six_Caps({ subsets: ['latin'], weight: ['400'] })
 
 function MyApp({ Component, pageProps }) {
