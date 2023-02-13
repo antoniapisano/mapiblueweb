cce890a54b927e2919f9ce9633a9c68e24f0817e
antoniapisano <antoniapisano@campus.masterschool.com>
2023-02-13 22:39:01 +0000

    layout almost done

--- pages/YoutubeEmbed.js
+++ pages/YoutubeEmbed.js
@@ -1,20 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from '../styles/Home.module.css'
 
 const YoutubeEmbed = ({ embedId }) => (
-  <div className="video-responsive">
     <iframe
-      width="853"
-      height="480"
-      left="30%"
-      right="30%"
+    className={styles.videoresponsive}
+      width="600"
+      height="350"
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
     />
-  </div>
 );
 
 YoutubeEmbed.propTypes = {
