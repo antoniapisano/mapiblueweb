068e6f818cfbb51dd8f752d49d2aff2d70dc1269
antoniapisano <antonia.a.pisano@gmail.com>
2023-11-29 00:06:13 +0000

    with navbar

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -5,13 +5,6 @@ import Navbar from "./Navbar";
 const Layout = ({ children }) => {
   return (
     <div className="page-container" >
-    <div className="backgroundimage">
-    <Image 
-        src="/tarotbackground.webp"
-        alt="https://unsplash.com/photos/king-of-diamonds-playing-card-7s2ip7OVktg"
-        fill cover
-      />
-    </div>
    <div className="content-wrap">
       <Navbar />
       {children}
