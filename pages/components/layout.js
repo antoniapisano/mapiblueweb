9509ab7caf9f5cb768dab91e58e0cebefd47d8fc
antoniapisano <antonia.a.pisano@gmail.com>
2023-12-09 22:27:24 +0000

    tarot card background

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -1,9 +1,17 @@
 import Footer from "./footer";
 import Navbar from "./Navbar";
+import Image from 'next/image';
 
 const Layout = ({ children }) => {
   return (
     <div className="page-container" >
+    <div className="backgroundimage">
+    <Image 
+        src="/tarotbackground.webp"
+        alt="https://unsplash.com/photos/king-of-diamonds-playing-card-7s2ip7OVktg"
+        fill cover
+      />
+    </div>
    <div className="content-wrap">
       <Navbar />
       {children}
@@ -12,5 +20,4 @@ const Layout = ({ children }) => {
     </div>
   );
 };
-
 export default Layout;
