4e726d3a1168960d8ccadc5f8024c0fad86b2756
antoniapisano <antonia.a.pisano@gmail.com>
2024-01-06 22:39:51 +0000

    black framer motion

--- pages/english/index.js
+++ pages/english/index.js
@@ -5,18 +5,31 @@ const indexeng = () => {
     return ( 
         <AnimatePresence mode="wait">
 
-        <h1>English page</h1>
+        <h1>Welcome</h1>
+        <div className="textbox">
+            <p>
+            I'm a successful Spanish Tarot Reader based in London. 
+            I'm here to guide you through life's most complex and difficult matters. 
+            Whatever you're going through, I'm here to help you find your way forward. 
+            From a young age I realized I was blessed with a gift that enabled me to see 
+            and sense things through Tarot that others could not.
+            </p>
+                <p>
+                I do Tarot readings in Spanish and English, in person or online. 
+                If you wish to schedule an appointment, you can contact me by email.
+                </p>
+        </div>
         <motion.div className="slide-in"
                 initial={{scaleY: 0}}
                 animate={{scaleY: 0}}
                 exit={{scaleY: 1}}
-                transition={{ duration: 1, ease: [0.22, 1, 0.36, 1]}}>
+                transition={{ duration: 5, ease: [2,2,2,2]}}>
     </motion.div>
     <motion.div className="slide-out"
                 initial={{scaleY: 1}}
                 animate={{scaleY: 0}}
                 exit={{scaleY: 0}}
-                transition={{ duration: 1, ease: [0.22, 1, 0.36, 1]}}></motion.div>
+                transition={{ duration: 5, ease: [2,2,2,2]}}></motion.div>
         </AnimatePresence>
      );
 }
--- pages/espanol/index.js
+++ pages/espanol/index.js
@@ -5,7 +5,20 @@ const indexesp = () => {
     return ( 
         <AnimatePresence mode="wait">
 
-        <h1>Spanish page</h1>
+<h1>Bienvenido</h1>
+        <div>
+            <p>
+            Soy una tarotista española viviendo en Londres. Estoy aquí para guiarte en materias
+             complejas y difíciles de la vida. Independientemente de lo que estés pasando, estoy
+              aquí para ayudarte y empoderarte a encontrar el mejor camino a seguir. 
+              Desde muy joven me di cuenta de que había sido bendecida con un don que me permitía
+               ver y sentir cosas a través del Tarot que otros no podían.
+            </p>
+                <p>
+                Hago lecturas de Tarot en español e inglés, presencial u online.
+                 Si desea reservar una cita, puede contactarme por email.
+                </p>
+        </div>
         <motion.div className="slide-in"
                 initial={{scaleY: 0}}
                 animate={{scaleY: 0}}
